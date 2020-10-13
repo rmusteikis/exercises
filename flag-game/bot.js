c7dd97c4d7db09a6a1c1438175c83928b93ca0af
Ramūnas Musteikis <ramunasm@wix.com>
2020-10-13 09:59:52 +0300

    clear bot's brain

--- flag-game/bot.js
+++ flag-game/bot.js
@@ -1,13 +1,6 @@
 // bot's brain
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
 function bot(state) {
   if (!state.player1 && state.flags.length) {
-    const pickNumber = getRandomInt(1, 3);
     setTimeout(() => {
       take(state.flags.length % 3);
       rerender(state);
