a30fa54e505397775f5862d6f3ca609ebf3589b2
Ramūnas Musteikis <ramunasm@wix.com>
2020-10-13 08:17:52 +0300

    add unbeatable bot

--- flag-game/bot.js
+++ flag-game/bot.js
@@ -9,13 +9,7 @@ function bot(state) {
   if (!state.player1 && state.flags.length) {
     const pickNumber = getRandomInt(1, 3);
     setTimeout(() => {
-      if (state.flags.length === 1) {
-        take(1);
-      } else if (state.flags.length === 2) {
-        take(2);
-      } else {
-        take(pickNumber);
-      }
+      take(state.flags.length % 3);
       rerender(state);
     }, 500);
   }
