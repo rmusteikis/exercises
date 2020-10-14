d9f3de1718689f2f136d8175c77ba2718e112d14
Ramūnas Musteikis <ramunasm@wix.com>
2020-10-14 16:39:02 +0300

    implement card validation

--- tdd-poker/index.js
+++ tdd-poker/index.js
@@ -1,5 +1,68 @@
-function comparePokerHands(a, b) {}
+const _ = require("lodash");
+
+function comparePokerHands(a, b) {
+  return true;
+}
+
+function validateHand(hand) {
+  if (typeof hand === "string") {
+    const handToString = hand.replace(/\s+/g, "");
+    const handArray = _.chunk(handToString, 2);
+    if (isEachCardValid(handArray)) {
+      return handArray;
+    }
+  } else {
+    return () => throwError();
+  }
+}
+
+function isEachCardValid(handArray) {
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "T",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  const cardSuits = ["S", "H", "D", "C"];
+  const isValidValues = [];
+  const isValidSuits = [];
+
+  for (let card of handArray) {
+    cardValues.includes(card[0]) ? isValidValues.push(true) : "";
+    cardSuits.includes(card[1]) ? isValidSuits.push(true) : "";
+  }
+
+  if (isValidValues.length === 5 && isValidSuits.length === 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function throwError() {
+  throw new Error("hand is invalid");
+}
 
 module.exports = {
   comparePokerHands,
+  validateHand,
+  isEachCardValid,
 };
+
+// const handArray = ["2H", "3H", "4H", "5H", "6H"];
+// const splitedHandArray = [
+// [2, "H"],
+// [3, "H"],
+// [4, "H"],
+// [5, "H"],
+// [6, "H"],
+// ];
--- tdd-poker/index.spec.js
+++ tdd-poker/index.spec.js
@@ -1,5 +1,51 @@
-const { comparePokerHands } = require("./index.js");
+const {
+  comparePokerHands,
+  validateHand,
+  isEachCardValid,
+} = require("./index.js");
 
-it("does nothing", () => {
-  expect(comparePokerHands(3, 4)).toBeTruthy();
+describe("comparePokerHands", () => {
+  it("should do nothing", () => {
+    const firstHand = "";
+    const secondHand = "";
+
+    expect(comparePokerHands(firstHand, secondHand)).toBeTruthy();
+  });
+
+  describe("The hand validation", () => {
+    let hand;
+    describe("when hand is not a string", () => {
+      it("should throw an error", () => {
+        hand = ["is", "not", "a", "string"];
+        expect(validateHand(hand)).toThrow();
+      });
+    });
+
+    describe("when hand is a valid string", () => {
+      it("should return an array", () => {
+        expect(validateHand(hand)).toBeInstanceOf(Array);
+      });
+
+      beforeEach(() => {
+        hand = "2H 3H 4H 5H 6H";
+      });
+
+      it("array should contain 5 items", () => {
+        expect(validateHand(hand)).toHaveLength(5);
+      });
+
+      it("each item should be an array", () => {
+        expect(validateHand(hand)[0]).toBeInstanceOf(Array);
+        expect(validateHand(hand)[1]).toBeInstanceOf(Array);
+        expect(validateHand(hand)[2]).toBeInstanceOf(Array);
+        expect(validateHand(hand)[3]).toBeInstanceOf(Array);
+        expect(validateHand(hand)[4]).toBeInstanceOf(Array);
+      });
+
+      it("each 'card' should exist in the deck", () => {
+        const handArray = validateHand(hand);
+        expect(isEachCardValid(handArray)).toBeTruthy();
+      });
+    });
+  });
 });
