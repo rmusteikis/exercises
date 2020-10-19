3c6b71c2fcabbee3a015736474ed45a558d7cd65
Ramūnas Musteikis <ramunasm@wix.com>
2020-10-19 13:41:35 +0300

    minor fixes

--- tdd-poker/README.md
+++ tdd-poker/README.md
@@ -1,11 +1,11 @@
 # TDD practice
-TDD is a *design process*, not a testing process. TDD is a robus way of designing software components ("units")
+TDD is a *design process*, not a testing process. TDD is a robust way of designing software components ("units")
 interactively so that their behaviour is specified through unit tests.
 
 **Test-first cycle**
 
 1. Write a simple failing test
-2. Make the test pass by writing the minimum amount of code, don't bother with code quality
+2. Make the test pass by writing the minimum amount of code, **don't bother with code quality**
 3. Refactor the code by applying design principles/patterns
 
 **Naming tests properly**
--- tdd-poker/index.spec.js
+++ tdd-poker/index.spec.js
@@ -12,7 +12,7 @@ describe("comparePokerHands", () => {
 
   describe("When first hand has higher rank", () => {
     it("should return 1", () => {
-      // expect.assertions(8);
+      expect.assertions(8);
       expect(comparePokerHands("2H 3H 4H 5H 6H", "AS AD AC AH JD")).toBe(1);
       expect(comparePokerHands("AS AH 2H AD AC", "JS JD JC JH 3D")).toBe(1);
       expect(comparePokerHands("2S AH 2H AS AC", "2H 3H 5H 6H 7H")).toBe(1);
@@ -26,6 +26,7 @@ describe("comparePokerHands", () => {
 
   describe("When first hand has lower rank", () => {
     it("should return -1", () => {
+      expect.assertions(6);
       expect(comparePokerHands("2H 3H 4H 5H 6H", "KS AS TS QS JS")).toBe(-1);
       expect(comparePokerHands("2S AH 2H AS AC", "JS JD JC JH AD")).toBe(-1);
       expect(comparePokerHands("2S 2H 4H 5S 4C", "AH AC 5H 6H AS")).toBe(-1);
